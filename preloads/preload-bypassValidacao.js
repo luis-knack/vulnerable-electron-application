545c45d2334ec81dd07f8a4496419ae71b192b08
Luis Tempest <luis.knack@tempest.com.br>
2021-05-11 10:29:00 -0300

    alteracoes preload script expondo apis

--- preloads/preload-bypassValidacao.js
+++ preloads/preload-bypassValidacao.js
@@ -2,16 +2,6 @@ var allowlist = [
     'https://www.safe.com',
 ];
 
-// function accessSite(arg){
-
-//     if(allowlist.includes(arg)){
-//         return "bypass"
-//     }
-//     return "failed"
-// }
-
-// window.accessSite=accessSite
-
 window.preloadAPI={
 
     accessSite(arg){
